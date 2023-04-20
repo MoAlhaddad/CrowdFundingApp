c2b190bc2abd513a6d1eb38989a94c5184d612e6
MoAlhaddad <mohamadalhaddad25@gmail.com>
2023-04-20 04:08:47 -0700

    part2 branch

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Sidebar from "./components/Sidebar";
+
+import {Route, Routes} from 'react-router-dom';
+const App = () => {
+  return (
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+        <div className="sm:flex hidden mr-10 relative">
+          <Sidebar/>
+        </div>
+    <div className="flex-1 max-sm-10">
+      Navbar 
+
+      <Routes>
+
+        </Routes>
+    </div>
+
+      App</div>
+  )
+}
+
+export default App
---
+++ client/src/assets/public/static/icons/index.js
@@ -0,0 +1,47 @@
+import Add from './add.svg';
+import Back from './back.svg';
+import Bell from './Bell.svg';
+import Close from './close.svg';
+import Expand from './expand.svg';
+import ExpandArrow from './ExpandArrow.svg';
+import Facebook from './facebook.svg';
+import FullScreen from './full-screen.svg';
+import Image from './image.svg';
+import Instagram from './instagram.svg';
+import Message from './Message.svg';
+import Media from './media.svg';
+import Play from './play.svg';
+import Plus from './plus.svg';
+import Save from './save.svg';
+import Search from './search.svg';
+import Share from './share.svg';
+import Sound from './sound.svg';
+import Twitter from './twitter.svg';
+import Upload from './Upload.svg';
+import Youtube from './youtube.svg';
+import Zoom from './zoom.svg';
+
+export {
+  Add,
+  Back,
+  Bell,
+  Close,
+  Expand,
+  ExpandArrow,
+  Facebook,
+  FullScreen,
+  Image,
+  Instagram,
+  Message,
+  Media,
+  Play,
+  Plus,
+  Save,
+  Search,
+  Share,
+  Sound,
+  Twitter,
+  Upload,
+  Youtube,
+  Zoom,
+};
